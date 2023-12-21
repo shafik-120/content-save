9e93ad3aee90325b6bc08f0816541bbb862a43c4
shafikul <shaifk108@gmail.com>
2023-12-21 15:49:50 +0600

    #1

---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="ContentEditableMain">
+    <div
+      contenteditable="true"
+      class="ContentEditable"
+      @input="inputValue"
+      ref="editableDiv"
+    ></div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const editableDiv = ref(null);
+
+onMounted(() => {
+  const savedData = localStorage.getItem('saveData');
+  editableDiv.value.innerHTML = savedData || ''; // Set content from localStorage or keep it empty
+});
+
+const inputValue = () => {
+  const content = editableDiv.value.innerHTML;
+  localStorage.setItem('saveData', content);
+};
+</script>
